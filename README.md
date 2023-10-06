c9ef32d29d348af004df16c25da87150c80163f3
Akele Belay <akelebch12@gmail.com>
2023-10-06 11:08:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# MERN Stack - Notes App 
+
+## Demo: http://rihana-note.ethiodevelopers.com/
+
+## Author: Akele Belay
+
+## Install dependencies for server 
+### `npm install`
+
+## Install dependencies for client
+### cd client ---> `npm install`
+
+## Connnect to your mongodb 
+
+## Run the client & server with concurrently
+### `npm run dev`
+
+## Run the Express server only
+### `npm run server`
+
+## Run the React client only
+### `npm run client`
+
+### Server runs on http://localhost:5000 and client on http://localhost:3000
+
+# Note:
+### For compateblity purpose, on package.json
+ use this: "start": "react-scripts --openssl-legacy-provider start",
+
---
+++ client/build/precache-manifest.535d83622cf5b0ca083d57b9e9085c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cb16842123794cace0a6186b16f823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ff31d761523808d59f",
+    "url": "/static/css/main.c7e87e46.chunk.css"
+  },
+  {
+    "revision": "08bfe183196f03331ccd",
+    "url": "/static/js/2.ed1285a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed1285a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ff31d761523808d59f",
+    "url": "/static/js/main.dce891aa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
